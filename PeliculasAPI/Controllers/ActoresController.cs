e983d28f0d2203418d7af1bf1f9a082cbf173b59
= <walterbc12@gmail.com>
2020-07-01 18:22:02 -0500

    Agregar archivos de proyecto.

---
+++ PeliculasAPI/Controllers/ActoresController.cs
@@ -0,0 +1,116 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Controllers
+{
+    [ApiController]
+    [Route("api/actores")]
+    public class ActoresController:ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public ActoresController(ApplicationDbContext context,
+            IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ActorDTO>>> Get()
+        {
+            var entidades = await context.Actores.ToListAsync();
+            return mapper.Map<List<ActorDTO>>(entidades);
+                     
+        }
+
+        [HttpGet("{id}", Name = "obtenerActor")]
+        public async Task<ActionResult<ActorDTO>> Get(int id)
+        {
+            var entidad = await context.Actores.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (entidad == null) { return NotFound(); }
+
+            return mapper.Map<ActorDTO>(entidad);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromForm] ActorCreacionDTO actorCreacionDTO)
+        {
+            var entidad = mapper.Map<Actor>(actorCreacionDTO);
+            context.Add(entidad);
+            await context.SaveChangesAsync();
+            var dto = mapper.Map<ActorDTO>(entidad);
+            return new CreatedAtRouteResult("obtenerActor", new { id = entidad.Id }, dto);          
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromForm] ActorCreacionDTO actorCreacionDTO)
+        {
+            var entidad = mapper.Map<Actor>(actorCreacionDTO);
+            entidad.Id = id;
+            context.Entry(entidad).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return NoContent();
+           
+        }
+
+        //[HttpPatch("{id}")]
+        //public async Task<ActionResult> Patch(int id, [FromBody] JsonPatchDocument<ActorPatchDTO> patchDocument)
+        //{
+        //    if (patchDocument == null)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    var entidadDB = await context.Actores.FirstOrDefaultAsync(x => x.Id == id);
+
+        //    if (entidadDB == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var entidadDTO = mapper.Map<ActorPatchDTO>(entidadDB);
+
+        //    patchDocument.ApplyTo(entidadDTO, ModelState);
+
+        //    var esValido = TryValidateModel(entidadDTO);
+
+        //    if (!esValido)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    mapper.Map(entidadDTO, entidadDB);
+
+        //    await context.SaveChangesAsync();
+
+        //    return NoContent();
+
+        //}
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await context.Actores.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Actor() { Id = id });
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
